a9b10ec985234fd783b4e427b0b464460d5378f6
vinicius <viniciusbgarcia.dev@gmail.com>
2024-10-11 20:54:11 -0300

    att services

--- src/services/petsService.ts
+++ src/services/petsService.ts
@@ -1,11 +1,51 @@
+import { Pet } from "../models/pet";
+
+const pets: Pet[] = [
+  {
+    id: 1,
+    name: "Rex",
+    age: 2,
+    breed: "labrador",
+    color: "Brown",
+    gender: "Male",
+  },
+  {
+    id: 2,
+    name: "Sparky",
+    age: 5,
+    breed: "Golden Retriever",
+    color: "Golden",
+    gender: "Male",
+  },
+];
+
+const notFound = new Error("pet not found");
+
 export class PetsService {
-  async fetchPets() {}
+  async fetchPets(): Promise<Pet[]> {
+    return pets;
+  }
 
-  async getPet(id) {}
+  async getPet(id: number): Promise<Pet | undefined> {
+    const pet = pets.find((p) => p.id === id);
+    if (pet === null) throw notFound;
+    return pet;
+  }
 
-  async createPet(pet) {}
+  async createPet(pet: Pet): Promise<Pet> {
+    pets.push(pet);
+    return pet;
+  }
 
-  async updatePet(id, pet) {}
+  async updatePet(id: number, pet: Pet): Promise<void> {
+    const index = pets.findIndex((p) => p.id === id);
+    if (index < 0) throw notFound;
+    pets[index] = pet;
+  }
 
-  async deletePet(id) {}
+  async deletePet(id: number): Promise<void> {
+    const index = pets.findIndex((p) => p.id === id);
+    if (index < 0) throw notFound;
+    pets.splice(index, 1);
+  }
 }
